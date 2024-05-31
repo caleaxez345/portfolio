356933be170115eb9c74dc5b623be2a13a9dfb11
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-05-30 22:22:02 -0400

    Installed type-writer effect, finished typewriter effect on home page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import Picture from '/Professional-Pic/professional-photo.jpg';
+import Typewriter from 'typewriter-effect';
 
 function Home() {
+    const typeWriter = (
+        <Typewriter
+            options={{
+                strings: ['Software Engineer',
+                    'CS Alumni @ UNC Charlotte',
+                    'Backend Developer',
+                    'Learning Front-end'
+                ],
+                autoStart: true,
+                loop: true,
+            }}
+        />
+    );
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -9,7 +24,10 @@ function Home() {
                 <div>
                     <h1 className="text-5xl font-bold">Welcome!</h1>
                     <h1 className="py-6 text-5xl font-bold">My name is Carlos Garcia Alavez!</h1>
-                    <p className="text-blue-400 text-3xl mb-4">Placeholder for typewriter text</p>
+                    {/* <p className="text-blue-400 text-3xl mb-4">Placeholder for typewriter text</p> */}
+                    <div className="text-blue-400 text-3xl mb-4"> 
+                        {typeWriter}
+                    </div>
                     <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black hover:bg-gray-200">View Resume</button>
                     <a href="/projects">
                         <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black ml-4 hover:bg-gray-200">View Projects</button>  
