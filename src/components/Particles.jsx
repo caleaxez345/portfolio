43191c54acec1af033964d6b961ba5551f187b6c
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-02 20:31:31 -0400

    Removed comments in Particles.jsx

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -544,59 +544,6 @@ function Particle() {
                 "value": true
               }
             }
-          // particles: {
-          //   move: {
-          //     direction: "none",
-          //     enable: true,
-          //     outModes: {
-          //       default: "bounce",
-          //     },
-          //     random: false,
-          //     speed: 6,
-          //     straight: false,
-          //   },
-          //   color: {
-          //       value: "#6B46C1"
-          //   },
-          //   number: {
-          //     density: {
-          //       enable: true,
-          //     },
-          //     value: 80,
-          //   },
-          //   opacity: {
-          //     value: 0.5,
-          //   },
-          //   shape: {
-          //     type: "circle",
-          //   },
-          //   size: {
-          //     value: { min: 1, max: 5 },
-          //   },
-          // },
-          // fpsLimit: 120,
-          // interactivity: {
-          //   events: {
-          //     onClick: {
-          //       enable: true,
-          //       mode: "push",
-          //     },
-          //     onHover: {
-          //       enable: true,
-          //       mode: "repulse",
-          //     },
-          //   },
-          //   modes: {
-          //     push: {
-          //       quantity: 4,
-          //     },
-          //     repulse: {
-          //       distance: 200,
-          //       duration: 0.4,
-          //     },
-          //   },
-          // },
-          // detectRetina: true,
         }),
         [],
       );
