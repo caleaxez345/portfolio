983ff68a4f376a05457bbeba0ce7deaf1eb2b05c
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-09 22:33:00 -0400

    Changed wording to Download Resume

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -27,7 +27,7 @@ function Home() {
                     <div className="text-blue-400 text-3xl mb-4"> 
                         {typeWriter}
                     </div>
-                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black hover:bg-gray-200">View Resume</button>
+                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black hover:bg-gray-200">Download Resume</button>
                     <a href="/projects">
                         <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black ml-4 hover:bg-gray-200">View Projects</button>  
                     </a>                     
