35eec5dd8166a2d7acb95122b095d980b5b5ccf3
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-12 21:51:46 -0400

    Added ScrollToTop component, whenever page is visited it always starts at the top

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,24 @@ import Contact from './pages/Contact';
 import AboutMe from './pages/AboutMe';
 import Projects from './pages/Projects';
 import Particle from './components/Particles';
+import ScrollToTop from './components/ScrollToTop';
 import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 
 function App() {
-  
   return (
     <>
       <BrowserRouter>
-        <Particle />
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Index />} />
-          <Route path='/about-me' element={<AboutMe />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
-        <Footer />
+        <ScrollToTop>
+          <Particle />
+            <Navbar />
+              <Routes>
+                <Route path='/' element={<Index />} />
+                <Route path='/about-me' element={<AboutMe />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/projects' element={<Projects />} />
+              </Routes>
+          <Footer />
+        </ScrollToTop>
       </BrowserRouter>
     </>
   )
