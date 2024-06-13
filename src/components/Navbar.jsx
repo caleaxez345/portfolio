54dfce2068069a219f52456ee6b24fc7874cc2aa
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-12 21:22:10 -0400

    Dropdown menu options are responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ function Navbar() {
                 <summary tabIndex={0} role="button" className="btn btn-ghost md:hidden"> 
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </summary>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 md:hidden">
                     <li><Link to='/' onClick={handleLinkClick}>Home</Link></li>
                     <li><Link to='/about-me' onClick={handleLinkClick}>About Me</Link></li>
                     <li><Link to='/contact' onClick={handleLinkClick}>Contact</Link></li>
