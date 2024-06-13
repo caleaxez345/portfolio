8f71152c3d30a0f86d837e55334d09b6373b21a7
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-13 12:17:53 -0400

    Basic outline of contact form with EmailJS

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
-function Contact() {
+const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+            publicKey: 'YOUR_PUBLIC_KEY',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
+
     return(
         <div className="w-full h-full">
             {/*Contact Page Header*/}
@@ -15,17 +35,19 @@ function Contact() {
                 {/*Right side of page, email form*/}
                 <div className="flex-1 border-2 border-blue-400 rounded-lg shadow-lg shadow-blue-400 bg-base-100 relative">
                     <h2 className="text-blue-400 text-3xl text-center mt-4">Send me an email! 🚀</h2>
-                    <div className="flex flex-col gap-3 mx-4 my-4">
-                        <div className="flex flex-row gap-2">
-                            <input type="text" name="from_name" placeholder="Full Name" className="input input-bordered w-full max-w-xs" />
-                            <input type="text" name="from_phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className="flex flex-col gap-3 mx-4 my-4">
+                            <div className="flex flex-row gap-2">
+                                <input type="text" name="from_name" placeholder="Full Name" className="input input-bordered w-full max-w-xs" />
+                                <input type="text" name="from_phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                            </div>
+                            <input type="email" name="from_email" placeholder="Email" className="input input-bordered w-full" />
+                            <textarea placeholder="Subject" name="message" className="input input-bordered w-full h-56 resize-none" />
+                            <div className="flex flex-row justify-center">
+                                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-white text-black border-2 border-blue-400 hover:bg-gray-200">Send</button>
+                            </div>
                         </div>
-                        <input type="email" name="from_email" placeholder="Email" className="input input-bordered w-full" />
-                        <textarea placeholder="Subject" name="message" className="input input-bordered w-full h-56 resize-none" />
-                        <div className="flex flex-row justify-center">
-                            <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-white text-black border-2 border-blue-400 hover:bg-gray-200">Send</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
