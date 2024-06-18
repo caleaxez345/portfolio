73da4f6a6306fec75a89dae8aa23dfe67f7892d6
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-18 12:20:36 -0400

    Added animations in the home page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -27,9 +27,9 @@ function Home() {
                     <div className="text-blue-400 text-3xl mb-4"> 
                         {typeWriter}
                     </div>
-                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black hover:bg-gray-200">Download Resume</button>
+                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black hover:bg-gray-200 hover:border-blue-400 hover:scale-105 hover:transition-all">Download Resume</button>
                     <a href="/projects">
-                        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black ml-4 hover:bg-gray-200">View Projects</button>  
+                        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 border-2 border-blue-400 bg-white text-black ml-4 hover:bg-gray-200 hover:border-blue-400 hover:scale-105 hover:transition-all">View Projects</button>  
                     </a>                     
                 </div>
             </div>
