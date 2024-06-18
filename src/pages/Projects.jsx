56aa1af850971eed8f9469c1588779e36cf851d7
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-18 12:00:14 -0400

    Added some animations to Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -17,13 +17,13 @@ function Projects() {
                 {/*Cards*/}
                 <div className="flex flex-wrap justify-center">
                     {projectsData.map((project, index) => (
-                        <div key={index} className="card w-96 bg-base-200 shadow-lg shadow-blue-400 mr-4 my-4 rounded-lg">
+                        <div key={index} className="card w-96 bg-base-200 shadow-lg shadow-blue-400 mr-4 my-4 rounded-lg border border-blue-400 hover:scale-105 hover:transition-all">
                             <figure><img src={project.image} alt={project.title} /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{project.title}</h2>
                                 <p className="">{project.description}</p>
                                 <div className="card-actions justify-center">
-                                    <a href={project.githubLink} className="btn text-black bg-white hover:bg-gray-200 border-2 border-blue-400" target="_blank" rel="noopener noreferrer">
+                                    <a href={project.githubLink} className="btn text-black bg-white hover:bg-gray-200 border-2 border-blue-400 hover:border-blue-400" target="_blank" rel="noopener noreferrer">
                                         <FaGithub size={20}/>Github
                                     </a>
                                 </div>
