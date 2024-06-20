41a66378e2b4e6a4a4fb2efe65e22ba41196dede
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-06-20 14:23:11 -0400

    Input validation for the contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,27 +7,69 @@ const Contact = () => {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [subject, setSubject] = useState('');
+    const [errors, setErrors] = useState('');
+
+    const validateForm = () => {
+        let formErrors = {};
+        let isValid = true;
+
+        {/*Input validation for full name. Check if it doesnt have letters or spaces between names*/}
+        if(!name) {
+            isValid = false;
+            formErrors['name'] = 'Name is required.';
+        }
+        else if(!/^[A-Za-z ]+$/.test(name.trim())) {
+            isValid = false;
+            formErrors['name'] = 'Invalid name.';
+        }
+
+        {/*Input validation for phone number. Check if it doesnt have numbers, dash, or spaces between numbers */}
+        if(!phoneNumber) {
+            isValid = false;
+            formErrors['phoneNumber'] = 'Phone number is required';
+        }
+        else if(!/^[0-9- ]+$/.test(phoneNumber.trim())) {
+            isValid=false;
+            formErrors['phoneNumber'] = 'Invalid phone number.';
+        }
+
+        {/*Input valiation if email doesnt exist*/}
+        if(!email) {
+            isValid = false;
+            formErrors['email'] = 'Email is required';
+        }
+
+        {/*Input validation if subject doesnt exists*/} 
+        if(!subject) {
+            isValid = false;
+            formErrors['subject'] = 'Subject is required';
+        }
+
+        setErrors(formErrors);
+        return isValid;
+    }
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        resetForm();
-    
-        emailjs
-          .sendForm(
-            import.meta.env.VITE_SERVICE_ID, 
-            import.meta.env.VITE_TEMPLATE_ID, 
-            form.current, {
-            publicKey: import.meta.env.VITE_PUBLIC_KEY,
-          })
-          .then(
-            () => {
-              console.log('SUCCESS!');
-            },
-            (error) => {
-              console.log('FAILED...', error.text);
-            },
-          );
+        if (validateForm()) {
+            resetForm();
+            emailjs
+                .sendForm(
+                    import.meta.env.VITE_SERVICE_ID,
+                    import.meta.env.VITE_TEMPLATE_ID,
+                    form.current, {
+                    publicKey: import.meta.env.VITE_PUBLIC_KEY,
+                })
+                .then(
+                    () => {
+                        console.log('SUCCESS!');
+                    },
+                    (error) => {
+                        console.log('FAILED...', error.text);
+                    },
+                );
+        }
     };
 
     const resetForm = () => {
@@ -54,43 +96,55 @@ const Contact = () => {
                     <h2 className="text-blue-400 text-3xl text-center mt-4">Send me an email! 🚀</h2>
                     <form ref={form} onSubmit={sendEmail}>
                         <div className="flex flex-col gap-3 mx-4 my-4">
-                            <input type="hidden" name="to_name" value="Carlos Garcia Alavez"/>
+                            <input type="hidden" name="to_name" value="Carlos Garcia Alavez" />
                             <div className="flex flex-row space-x-4 justify-between">
-                                <input 
-                                    type="text"
-                                    name="from_name" 
-                                    placeholder="Full Name" 
-                                    className="input input-bordered w-full" 
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                    required
-                                />
-                                <input type="text" 
-                                    name="from_phone" 
-                                    placeholder="Phone Number" 
-                                    className="input input-bordered w-full" 
-                                    value={phoneNumber}
-                                    onChange={(e) => setPhoneNumber(e.target.value)}
-                                    required
-                                />
+                                <div className="w-full flex flex-col">
+                                    <input
+                                        type="text"
+                                        name="from_name"
+                                        placeholder="Full Name"
+                                        className="input input-bordered w-full"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        required
+                                    />
+                                    {errors.name && <p className="text-red-500 text-center">{errors.name}</p>}
+                                </div>
+                                <div className="w-full flex flex-col">
+                                    <input type="text"
+                                        name="from_phone"
+                                        placeholder="Phone Number"
+                                        className="input input-bordered w-full"
+                                        value={phoneNumber}
+                                        onChange={(e) => setPhoneNumber(e.target.value)}
+                                        required
+                                    />
+                                    {errors.phoneNumber && <p className="text-red-500 text-center">{errors.phoneNumber}</p>}
+                                </div>
                             </div>
-                            <input type="email" 
-                                name="from_email" 
-                                placeholder="Email" 
-                                className="input input-bordered w-full" 
-                                value={email} 
-                                onChange={(e) => setEmail(e.target.value)} 
-                                required
-                            />
+                            <div className="w-full flex flex-col">
+                                <input type="email" 
+                                    name="from_email" 
+                                    placeholder="Email" 
+                                    className="input input-bordered w-full" 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    required
+                                />
+                                {errors.email && <p className="text-red-500 text-center">{errors.email}</p>}
+                            </div>
                             <input type="hidden" name="reply_to" value={email}/>
-                            <textarea 
-                                placeholder="Subject" 
-                                name="message" 
-                                className="input input-bordered w-full h-56 resize-none" 
-                                value={subject}
-                                onChange={(e) => setSubject(e.target.value)}
-                                required
-                            />
+                            <div className="w-full flex flex-col">
+                                <textarea 
+                                    placeholder="Subject" 
+                                    name="message" 
+                                    className="input input-bordered w-full h-56 resize-none" 
+                                    value={subject}
+                                    onChange={(e) => setSubject(e.target.value)}
+                                    required
+                                />
+                                {errors.subject && <p className="text-red-500 text-center">{errors.subject}</p>}
+                            </div>
                             <div className="flex flex-row justify-center">
                                 <button 
                                     type="submit" 
