7400f8e227a17117fb79c5752e08bcb43beb4cad
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-07-06 20:25:33 -0400

    About me page is fully responsive

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -10,45 +10,46 @@ function AboutMe() {
       {/*Header*/}
       <div className="container mx-auto my-4">
         <div className="text-center">
-          <h1 className="text-2xl font-bold text-blue-400">About Me</h1>
+          <h1 className="text-2xl lg:text-3xl font-bold text-blue-400">About Me</h1>
         </div>
       </div>
       {/*Flex box for right and left side of the page*/}
-      <div className="flex flex-col lg:flex-row mx-4">
+      <div className="flex flex-col md:flex-row mx-4">
         {/*flex box for left side of the page*/}
         <div className="flex-1 flex items-center justify-center">
           <div className="w-full p-4">
-            <h2 className="text-xl font-bold mb-2 text-blue-400 text-center">Get to know who I am!</h2>
-            <p className="text-lg">Hello everyone, I'm a 22 year old Software Engineer from Charlotte, NC.</p>
-            <p className="text-lg">I started coding once I began College and I was just so interested in being able to create new applications and face those tough challenges that come with it and learn new technologies.</p>
-            <p className="text-lg my-4">Now, apart from coding Here are some of my hobbies that I like to take part in!</p>
+            <h2 className="text-xl lg:text-2xl font-bold mb-2 text-blue-400 text-center">Get to know who I am!</h2>
+            <p className="text-lg lg:text-xl">Hello everyone, I'm a 22 year old Software Engineer from Charlotte, NC.</p>
+            <p className="text-lg lg:text-xl">I started coding once I began College and I was just so interested in being able to create new applications and face those tough challenges that come with it and learn new technologies.</p>
+            <p className="text-lg lg:text-xl my-4">Now, apart from coding Here are some of my hobbies that I like to take part in!</p>
             <div className="flex justify-center">
-              <ol class="list-disc text-center">
-                <li className="text-lg">Video Games</li>
-                <li className="text-lg">Lifting Weights</li>
-                <li className="text-lg">Traveling</li>
+              <ol class="list-disc text-blue-400">
+                <li className="text-lg lg:text-xl">Video Games</li>
+                <li className="text-lg lg:text-xl">Lifting Weights</li>
+                <li className="text-lg lg:text-xl">Traveling</li>
               </ol>
             </div>
           </div>
         </div>
         {/*flex box for right side of the page*/}
-        <div className="flex-1 z-10">
-          <div className="w-full flex flex-col justify-center items-center border-2 border-blue-400">
-            <Lottie animationData={animationData} className="h-[500px] w-full" />
+        <div className="z-10 flex-1">
+          <div className="w-full flex flex-col justify-start items-center">
+            <Lottie animationData={animationData} className="h-full w-full md:h-[350px]" />
           </div>
         </div>
       </div>
       {/*Professional skillset container*/}
       <div className="container mx-auto my-5">
         <h2 className="text-2xl font-bold mb-4 mt-4 text-blue-400 text-center">Professional Skillset</h2>
-        <h3 className="text-lg text-center underline">Languages</h3>
+        <h3 className="text-xl text-center underline">Languages</h3>
         {/*Language boxes*/}
         <div className="flex flex-wrap justify-center">
+        {/* <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-0 justify-center"> */}
           {programmingLanguages.item.map((language, index) => (
             <div
               key={index}
-              className="w-24 h-24 lg:w-60 bg-base-100 border border-blue-400 mx-4 my-4 p-4 flex flex-col items-center hover:scale-105 hover:transition-all">
-              <h2 className="text-center"> {language.name} </h2>
+              className="w-36 h-28 lg:w-44 lg:h-32 xl:w-48 xl:h-36 bg-base-100 border border-blue-400 mx-4 my-4 p-4 lg:p-6 xl:p-8 flex flex-col items-center hover:scale-105 hover:transition-all">
+              <h2 className="text-center lg:text-lg"> {language.name} </h2>
               <language.icon size={48} />
             </div>
           ))}
@@ -57,8 +58,8 @@ function AboutMe() {
         {/*Database boxes*/}
         <div className="flex flex-wrap justify-center">
           {databases.item.map((database, index) => ( 
-            <div key={index} className="w-24 h-24 lg:w-60 bg-base-100 border border-blue-400 mx-4 my-4 p-4 flex flex-col items-center hover:scale-105 hover:transition-all"> 
-            <h2 className="text-center">{database.name}</h2>
+            <div key={index} className="w-36 h-28 lg:w-44 lg:h-32 xl:w-48 xl:h-36 bg-base-100 border border-blue-400 mx-4 my-4 p-4 lg:p-6 xl:p-8 flex flex-col items-center hover:scale-105 hover:transition-all"> 
+            <h2 className="text-center lg:text-lg">{database.name}</h2>
             <database.icon size={48} />
             </div>
           ))}
@@ -67,8 +68,8 @@ function AboutMe() {
         {/*Framework boxes*/}
         <div className="flex flex-wrap justify-center">
           {frameworks.item.map((framework, index) => ( 
-            <div key={index} className="w-24 h-24 lg:w-60 bg-base-100 border border-blue-400 mx-4 my-4 p-2 flex flex-col items-center hover:scale-105 hover:transition-all"> 
-            <h2 className="text-center">{framework.name}</h2>
+            <div key={index} className="w-36 h-28 lg:w-44 lg:h-32 xl:w-48 xl:h-36 bg-base-100 border border-blue-400 mx-4 my-4 p-4 lg:p-6 xl:p-8 flex flex-col items-center hover:scale-105 hover:transition-all"> 
+            <h2 className="text-center lg:text-lg">{framework.name}</h2>
             <framework.icon size={48} />
             </div>
           ))}
