a2538d962c99405366d13294c9bcd50176b3d915
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-07-10 13:48:12 -0400

    ProjectCard component refactoring

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -12,14 +12,13 @@ const ProjectCard = ({ project, index }) => {
             ref={ref}
             initial={{ opacity: 0, scale: 0 }}
             animate={isInView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }}
-            transition={{ duration: 0.3, delay: index * 0.1 }}
-            whileHover = {{ scale: 1.1 }}
-            whileTap={{ scale: 1.1}}
+            transition={{ duration: 0.3 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
             className="card w-96 bg-base-200 shadow-lg shadow-blue-400 mx-6 my-10 rounded-lg border border-blue-400"
         >
             <figure><img src={project.image} alt={project.title} /></figure>
-            <motion
-            className="card-body">
+            <div className="card-body">
                 <h2 className="card-title">{project.title}</h2>
                 <p className="">{project.description}</p>
                 <div className="card-actions justify-center">
@@ -27,7 +26,7 @@ const ProjectCard = ({ project, index }) => {
                         <FaGithub size={20}/>Github
                     </a>
                 </div>
-            </motion>
+            </div>
         </motion.div>
     );
 };
