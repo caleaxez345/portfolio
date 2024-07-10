df501335990a6ceb6f8629db3371eca3d04d617b
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-07-10 03:23:14 -0400

    Added animations for the Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import projectsData from '../data/projectsData';
 import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
 
-
-function Projects() {
+const Projects = () => {
     return (
         <>
             <div className="w-full h-full">
@@ -17,23 +17,29 @@ function Projects() {
                 {/*Cards*/}
                 <div className="flex flex-col md:flex-row items-center justify-center">
                     {projectsData.map((project, index) => (
-                        <div key={index} className="card w-96 bg-base-200 shadow-lg shadow-blue-400 mr-4 my-4 rounded-lg border border-blue-400 hover:scale-105 hover:transition-all">
-                            <figure><img src={project.image} alt={project.title} /></figure>
-                            <div className="card-body">
-                                <h2 className="card-title">{project.title}</h2>
-                                <p className="">{project.description}</p>
-                                <div className="card-actions justify-center">
-                                    <a href={project.githubLink} className="btn text-black bg-white hover:bg-gray-200 border-2 border-blue-400 hover:border-blue-400" target="_blank" rel="noopener noreferrer">
-                                        <FaGithub size={20}/>Github
-                                    </a>
+                        <motion.div 
+                           initial={{ opacity: 0, y: 50 }}
+                           whileInView={{ opacity: 1, y: 0 }}
+                           viewport={{ once: true, amount: 0.8 }}
+                           transition={{ duration: 0.3, delay: index * 0.1 }}
+                            key={index} 
+                            className="card w-96 bg-base-200 shadow-lg shadow-blue-400 mr-4 my-4 rounded-lg border border-blue-400">
+                                <figure><img src={project.image} alt={project.title} /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{project.title}</h2>
+                                    <p className="">{project.description}</p>
+                                    <div className="card-actions justify-center">
+                                        <a href={project.githubLink} className="btn text-black bg-white hover:bg-gray-200 border-2 border-blue-400 hover:border-blue-400" target="_blank" rel="noopener noreferrer">
+                                            <FaGithub size={20}/>Github
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    ))}
+                        </motion.div>
+                        ))}
                 </div >
             </div>
         </>
     );
-}
+};
 
 export default Projects;
